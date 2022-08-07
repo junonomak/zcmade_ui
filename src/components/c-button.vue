26503315f35753eed114dc3e00d529ad01b4f9e1
junonomak <www.991430373@qq.com>
2022-08-07 23:47:41 +0800

    group-button完成

--- src/components/c-button.vue
+++ src/components/c-button.vue
@@ -1,22 +1,42 @@
 <template>
-    <button class="c-button" :class="{[`icon-${iconPosition}`]: true}">
-        <i :class="`cmade-icon ${icon}`"></i>
-            <slot></slot>
+    <button class="c-button" :class="{[`icon-${iconPosition}`]: true}" @click="loadingCheck">
+        <!-- <i :class="{[`cmade-icon ${iconname}`]: true, 'button-loading': loading}"></i> -->
+        <c-icon :class="{'button-loading': iconname === 'i-loading'}" :name="iconname"></c-icon>
+        <slot></slot>
     </button>
 </template>
 
 <script setup>
-import {defineProps} from 'vue'
+import cIcon from '../components/c-icon.vue'
+import {defineProps, ref} from 'vue'
 const props = defineProps({
     icon: String,
-    iconPosition: String
+    iconPosition: String,
+    loading: Boolean
 })
+const iconname = ref(props.icon)
 
-console.log(props.iconPosition);
+
+// 判断传入参数并作出改变
+const init = () => {
+}
+init()
+
+const loadingCheck = () => {
+    if(props.loading === true){
+        iconname.value = 'i-loading'
+    }
+}
+
+
 
 </script>
 
 <style scpoed>
+    @keyframes spin{
+        0%{ transform: rotate(0deg); }
+        100%{ transform: rotate(360deg); }
+    }
     :root{
         --button-height: 32px;
         --font-size: 14px;
@@ -34,6 +54,10 @@ console.log(props.iconPosition);
         border: 1px solid var(--color);
         border-radius: var(--border-radius); 
         background-color: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
     }
     .c-button:hover{
         border-color: var(--boder-color-hover);
@@ -47,18 +71,15 @@ console.log(props.iconPosition);
     .cmade-icon{
         margin-right: .1em;
     }
-    .icon-right{
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-    }
+
     .icon-right > .cmade-icon{
         order: 2;
         margin-left: .1em;
         margin-right: 0 ! important;
     }
-
-
+    .button-loading{
+        animation: spin 2s infinite linear;
+    }
 
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
     <div class="content">
         <c-button icon="i-like" iconPosition="right" >设置</c-button>
-        <c-button>设置</c-button>
-        <c-button>设置</c-button>
-        <c-button>设置</c-button>
+        <c-button :loading="true" iconPosition="right" icon="i-like">加载中</c-button>
+
+    <div>
+        <c-group-button>
+            <c-button icon="i-left">上一页</c-button>
+            <c-button >设置</c-button>
+            <c-button icon="i-right" iconPosition="right">下一页</c-button>
+        </c-group-button>
+    </div>
+
     </div>
     
 </template>
 
 <script setup>
 import cButton from '../components/c-button.vue'
-
-
+import cIcon from '../components/c-icon.vue'
+import cGroupButton from '../components/c-group-button.vue'
 
 </script>
 
