40f407130a4d782134605af61bb34843ef082c5e
junonomak <www.991430373@qq.com>
2022-08-10 14:46:06 +0800

    input做好了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,31 @@
                 <c-button icon="i-right" iconPosition="right">下一页</c-button>
             </c-group-button>
         </div>
-        <c-input></c-input>
+        <div style="margin-top: 30px">
+            <c-input modelValue="一号" disabled></c-input>
+            <c-input modelValue="二号" readonly></c-input>
+            <c-input modelValue="三号" error="用户名格式错误"></c-input>
+            <c-input @input="changeValue" modelValue="我绑定了@input事件"></c-input>
+            <c-input v-model="sayhi"></c-input>
+            <c-input modelValue="五号"></c-input>
+            <h2>{{sayhi}}</h2>
+        </div>
     </div>
     
 </template>
 
 <script setup>
+import {ref} from 'vue'
+
+const sayhi = ref('我是v-model')
+setInterval(()=>{
+    sayhi.value += '1'
+},1000)
+
+
+const changeValue = (e) => {
+    console.log(e);
+}
 </script>
 
 <style scpoed>
